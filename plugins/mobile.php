a8f24d0f6c29d4c3ee3236c6275b6e908569b3a9
Matt Wiebe <matt@somadesign.ca>
2010-09-11 20:55:46 -0500

    YMMT (yet more mobile tweaks)

--- plugins/mobile.php
+++ plugins/mobile.php
@@ -2,5 +2,5 @@
 
 add_action('wp_head', 'sd_viewport');
 function sd_viewport() { ?>
-<meta name="viewport" content="width=device-width; initial-scale=1.0">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0">
 <?php }
